e69d1ff42c7bf125086b510244b74010ef15735c
LegendaLon <56084372+LegendaLon@users.noreply.github.com>
2024-03-08 01:18:02 +0200

    New blocks

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,10 @@
 $(() => {
+  // Отрисовка блоков
   $('#blockItemTemplate').tmpl(BlockData.program).appendTo('#programBlock');
 
+  // Отрисовка блоков
   $('#blockItemTemplate').tmpl(BlockData.pins).appendTo('#pinsBlock');
+
+  // Отрисовка блоков
+  $('#blockItemTemplate').tmpl(BlockData.mathoperators).appendTo('#mathoperatorsBlock');
 })
