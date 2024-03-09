def2bac20d096eb01e87e87d72b8d6bf1b177428
LegendaLon <56084372+LegendaLon@users.noreply.github.com>
2024-03-09 10:52:15 +0200

    Generic Hardware

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,13 @@
 $(() => {
   // Отрисовка блоков
-  $('#blockItemTemplate').tmpl(BlockData.program).appendTo('#programBlock');
+  $('#blockItemTemplate').tmpl(BlockData.program).appendTo('#contorlBlock');
 
   // Отрисовка блоков
   $('#blockItemTemplate').tmpl(BlockData.pins).appendTo('#pinsBlock');
 
   // Отрисовка блоков
   $('#blockItemTemplate').tmpl(BlockData.mathoperators).appendTo('#mathoperatorsBlock');
+
+  // Отрисовка блоков
+  $('#blockItemTemplate').tmpl(BlockData.generichardware).appendTo('#generichardwareBlock');
 })
